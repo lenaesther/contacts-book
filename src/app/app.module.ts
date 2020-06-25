995eddda3f078d0f94ea75896e4f11b7e4365119
Lena Wusits <lena.e.wusits@gmail.com>
2020-06-25 13:50:33 +0200

    - styling - add bootstrap and font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
@@ -9,6 +8,7 @@ import { ContactListComponent } from './components/contact-list/contact-list.com
 import { ContactDetailComponent } from './components/contact-detail/contact-detail.component';
 import { AddContactComponent } from './components/add-contact/add-contact.component';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -24,6 +24,7 @@ import { FormsModule } from '@angular/forms';
       contactState: contactReducer
     }),
     FormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
